c31df2ca70c34d406b44a3c3e355705d566ae037
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-07-05 17:44:14 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ export const config = {
   sessionFolder: "./session",
   pluginsFolder: "./plugins",
 
+  newsletterJid: "0029Vb8G49lKmCPR44sIOE1i@newsletter",
+
   apis: {
     edward: {
       baseUrl: "https://dv-edward.onrender.com",
