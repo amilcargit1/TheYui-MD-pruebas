788a18f5c6c06123460cc991c071d79e5bf18d20
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-07-12 09:31:17 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -84,7 +84,7 @@ export default {
     texto += `│ ⚡ ${totalComandos} cmd  │ 📦 ${allPlugins.length} plugins\n`;
     texto += `│ 🕐 ${fecha}\n`;
     texto += `│ 🌐 Powerby:𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐋𝐋𝐂`;   
-    texto += `╰────────────────────╯\n\n`;
+    texto += `╰───────────────╯\n\n`;
 
     texto += `🌹 *MIS COMANDOS* 🦋`;
     texto += `🕹 𝐈𝐀 𝐄𝐍 𝐃𝐄𝐒𝐀𝐑𝐑𝐎𝐋𝐋𝐎 📡\n`;
